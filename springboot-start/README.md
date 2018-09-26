bbb6983425fdf5fae26ca722c875976107454e2e
mingzhi.xie <mingzhi.xie@yintech.cn>
2018-09-26 12:37:26 +0800

    springboot-start 入门

---
+++ springboot-start/README.md
@@ -0,0 +1,2 @@
+# SpringBoot-start
+SpringBoot入门
---
+++ springboot-start/src/main/java/com/miven/springboot/start/Example.java
@@ -0,0 +1,22 @@
+package com.miven.springboot.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mingzhi.xie on 2018/9/25.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
