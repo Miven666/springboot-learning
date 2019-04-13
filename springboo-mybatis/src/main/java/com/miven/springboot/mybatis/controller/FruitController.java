133887241ad13ba9635faa7e782d2b6744e8fb6a
miven <xie6032mail@qq.com>
2019-04-13 21:37:08 +0800

    Add two param example

--- springboo-mybatis/src/main/java/com/miven/springboot/mybatis/controller/FruitController.java
+++ springboo-mybatis/src/main/java/com/miven/springboot/mybatis/controller/FruitController.java
@@ -1,10 +1,12 @@
 package com.miven.springboot.mybatis.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.miven.springboot.mybatis.entity.Fruit;
 import com.miven.springboot.mybatis.mapper.FruitMapper;
 import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -28,5 +30,12 @@ public class FruitController {
         fruit.setNumber(10);
         fruitMapper.insert(fruit);
         return JSONParser.quote("OK");
+    }
+
+    @GetMapping("/select")
+    public String select(@RequestParam("name") String name) {
+        Fruit fruit = new Fruit();
+        fruit.setName(name);
+        return JSON.toJSONString(fruitMapper.select(fruit, "name"));
     }
 }
