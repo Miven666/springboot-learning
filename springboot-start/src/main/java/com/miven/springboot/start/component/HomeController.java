d1706ca5135aa18085f82375c7378c07a50ad80e
mingzhi.xie <xie6032mail@163.com>
2019-08-20 16:16:36 +0800

    Add get body

--- springboot-start/src/main/java/com/miven/springboot/start/component/HomeController.java
+++ springboot-start/src/main/java/com/miven/springboot/start/component/HomeController.java
@@ -1,23 +1,47 @@
 package com.miven.springboot.start.component;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.miven.springboot.start.pojo.Home;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
- *
+ * 控制层
  * @author mingzhi.xie
  * @date 2018/9/27
  */
+@Slf4j
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
-    @Autowired
+    @Resource
     private HomeService homeService;
 
+    /**
+     *  未指定请求方式，get、post都可以
+     * @return String 默认"Content-Type: text/plain;charset=UTF-8"
+     */
     @RequestMapping("/door")
-    String door() {
+    public String door() {
+        return homeService.getHome();
+    }
+
+    /**
+     * get请求一般状况下无请求体，但是也是可以有请求体的
+     * @param home 请求体
+     */
+    @GetMapping(value = "/desk")
+    public String desk(@RequestBody Home home) {
+        log.info(home.toString());
+        return homeService.getHome();
+    }
+
+    @PostMapping(value = "/window", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String window(@RequestBody Home home) {
+        log.info(home.toString());
         return homeService.getHome();
     }
 }
