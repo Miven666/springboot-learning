31a9eb596b74124f40cbbf04c01ef7e4bbdd9ae2
mingzhi.xie <xie6032mail@163.com>
2019-09-02 11:12:15 +0800

    Update cache with put and evict

--- springboot-cache/src/main/java/com/miven/springboot/cache/BookRepository.java
+++ springboot-cache/src/main/java/com/miven/springboot/cache/BookRepository.java
@@ -1,6 +1,9 @@
 package com.miven.springboot.cache;
 
 import com.miven.entity.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
@@ -10,17 +13,50 @@ import org.springframework.stereotype.Repository;
  * @date 2019/8/28
  * @since 1.0
  */
+@Slf4j
 @Repository
 public class BookRepository {
 
-    @Cacheable("books")
-    public Book getByIsbn(String isbn) {
+    @Cacheable(value = "some book", key = "#p0")
+    public Book getByIsbn(String isbn, String title) {
         try {
-            Thread.sleep(5000L);
+            Thread.sleep(2000L);
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
 
-        return  new Book(isbn, "Some Book");
+        return  new Book(isbn, title);
+    }
+
+    @Cacheable(cacheNames = "one book", key = "#p0")
+    public Book getBook(String isbn, String title) {
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return  new Book(isbn, title);
+    }
+
+    @CachePut(cacheNames = "one book", key = "#p0")
+    public Book putBook(String isbn, String title) {
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return  new Book(isbn, title);
+    }
+
+    @CacheEvict(cacheNames = "some book", key = "#p0", allEntries = true, beforeInvocation = true)
+    public void removeBook(String isbn, String title) {
+        try {
+            Thread.sleep(2000L);
+            log.info("Removing " + title + " with isbn is "+ isbn);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
--- springboot-cache/src/main/java/com/miven/springboot/cache/CacheConfiguration.java
+++ springboot-cache/src/main/java/com/miven/springboot/cache/CacheConfiguration.java
@@ -17,13 +17,29 @@ public class CacheConfiguration {
     @Bean
     public CommandLineRunner runner(BookRepository bookRepository) {
         return args -> {
-            log.info(".... Fetching books");
-            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
-            log.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
-            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
-            log.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
-            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
-            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+            log.info(".... Fetching Some Books");
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234", "Some Book"));
+            log.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567", "Some Book"));
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234", "Some Book"));
+            log.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567", "Some Book"));
+
+            log.info(".... Fetching One Book");
+            log.info("isbn-1234 -->" + bookRepository.getBook("isbn-1234", "One Book"));
+            log.info("isbn-4567 -->" + bookRepository.getBook("isbn-4567", "One Book"));
+            log.info("isbn-1234 -->" + bookRepository.getBook("isbn-1234", "One Book"));
+            log.info("isbn-4567 -->" + bookRepository.getBook("isbn-4567", "One Book"));
+
+            log.info(".... Putting One Book");
+            log.info("isbn-1234 -->" + bookRepository.putBook("isbn-1234", "One Book"));
+            log.info("isbn-4567 -->" + bookRepository.putBook("isbn-4567", "One Book"));
+            log.info("isbn-1234 -->" + bookRepository.putBook("isbn-1234", "One Book"));
+            log.info("isbn-4567 -->" + bookRepository.putBook("isbn-4567", "One Book"));
+
+            log.info(".... Removing Some Book");
+            bookRepository.removeBook("isbn-1234", "Some Book");
+            bookRepository.removeBook("isbn-4567", "Some Book");
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234", "Some Book"));
+            log.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567", "Some Book"));
         };
     }
 }
