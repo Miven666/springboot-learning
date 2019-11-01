709f5e753999d6718e3b0712274cd7b94dc7ec6f
mingzhi.xie <xie6032mail@163.com>
2019-11-01 19:07:10 +0800

    更新 单元测试

--- springboot-test/src/test/java/com/springboot/test/TestLauncherTest.java
+++ springboot-test/src/test/java/com/springboot/test/TestLauncherTest.java
@@ -2,12 +2,13 @@ package com.springboot.test;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
+import static org.junit.runners.MethodSorters.NAME_ASCENDING;
+
 @SpringBootTest(classes = TestLauncher.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@FixMethodOrder(NAME_ASCENDING)
 public class TestLauncherTest extends AbstractJUnit4SpringContextTests {
 
     @Test
