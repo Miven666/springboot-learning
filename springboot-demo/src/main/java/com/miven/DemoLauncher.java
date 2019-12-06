461685935c674dba1ebab26acfc6f04adb1d3754
mingzhi.xie <xie6032mail@163.com>
2019-12-06 16:14:09 +0800

    获取项目版本

--- springboot-demo/src/main/java/com/miven/DemoLauncher.java
+++ springboot-demo/src/main/java/com/miven/DemoLauncher.java
@@ -1,23 +1,30 @@
 package com.miven;
 
 import com.miven.spring.context.XmzComponentScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  *  演示样例启动器
  * @author mingzhi.xie
  * @date 2018/9/27
  */
-@ComponentScan
-@EnableAutoConfiguration
-@SpringBootConfiguration
+@SpringBootApplication
 @XmzComponentScan
 public class DemoLauncher {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoLauncher.class,args);
+    }
+
+    @Value("${project.version}")
+    private String version;
+
+    @Bean
+    ApplicationRunner version() {
+        return args -> System.out.println(version);
     }
 }
