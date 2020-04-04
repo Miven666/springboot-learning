4740d9cdb53d644c5733d3e9adb0db7a4e2c9b44
mingzhi.xie <xie6032mail@163.com>
2020-04-04 17:30:59 +0800

    新增classloader查看

--- springboot-demo/src/main/java/com/miven/DemoLauncher.java
+++ springboot-demo/src/main/java/com/miven/DemoLauncher.java
@@ -1,11 +1,14 @@
 package com.miven;
 
 import com.miven.spring.context.XmzComponentScan;
+import org.hibernate.validator.HibernateValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.validation.spi.ValidationProvider;
 
 /**
  *  演示样例启动器
@@ -17,6 +20,20 @@ import org.springframework.context.annotation.Bean;
 public class DemoLauncher {
 
     public static void main(String[] args) {
+        ClassLoader classLoader = Object.class.getClassLoader();
+        System.out.println(classLoader);
+
+        ClassLoader loader = DemoLauncher.class.getClassLoader();
+        System.out.println(loader.getClass().getName());
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println(contextClassLoader.getClass().getName());
+
+        ClassLoader val = ValidationProvider.class.getClassLoader();
+        System.out.println("ValidationProvider -> " + val.getClass().getName());
+
+        ClassLoader hi = HibernateValidator.class.getClassLoader();
+        System.out.println("HibernateValidator -> " + hi.getClass().getName());
+
         SpringApplication.run(DemoLauncher.class,args);
     }
 
--- springboot-demo/src/main/java/com/miven/spring/web/RestConfiguration.java
+++ springboot-demo/src/main/java/com/miven/spring/web/RestConfiguration.java
@@ -1,16 +1,8 @@
 package com.miven.spring.web;
 
-import com.miven.entity.Fruit;
-import com.miven.spring31.web.DefaultClientHttpRequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.Assert;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -24,35 +16,4 @@ import org.springframework.web.client.RestTemplate;
 @ConditionalOnClass(RestTemplate.class)
 public class RestConfiguration {
 
-    private DefaultClientHttpRequestInterceptor interceptor;
-
-    /**
-     *  给RestTemplate添加一个默认的http请求拦截器，并注册
-     * @param builder 由{@link RestTemplateAutoConfiguration} 注册
-     */
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        this.interceptor = new DefaultClientHttpRequestInterceptor();
-        return builder.additionalInterceptors(this.interceptor).build();
-    }
-
-    /**
-     *  程序启动后执行的操作（发送一个http请求，并打印响应结果）
-     * @param restTemplate 注入注册的restTemplate
-     * @return 注册的CommandLineRunner
-     */
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) {
-        return args -> {
-            Fruit fruit;
-            try {
-                fruit = restTemplate.getForObject("http://localhost:8081/app/fruit/select?name=apple", Fruit.class);
-                Assert.notNull(fruit, "fruit must not be null");
-                log.info("httpHeaders has contains key 'Authorization': {}", this.interceptor.containsKey("Authorization"));
-                log.info(fruit.toString());
-            } catch (RestClientException e) {
-                log.error("Remote call failed: {}", e.getMessage());
-            }
-        };
-    }
 }
