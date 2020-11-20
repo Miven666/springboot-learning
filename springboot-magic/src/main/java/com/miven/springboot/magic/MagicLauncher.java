a97a19dbc48e176304cc9c23434e4258ee9a5aa3
mingzhi.xie <xie6032mail@163.com>
2020-11-20 11:51:42 +0800

    新增 springboot magic 模块

---
+++ springboot-magic/src/main/java/com/miven/springboot/magic/MagicLauncher.java
@@ -0,0 +1,16 @@
+package com.miven.springboot.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mingzhi.xie
+ * @since 2020/11/20 1.0
+ */
+@SpringBootApplication
+public class MagicLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicLauncher.class, args);
+    }
+}
